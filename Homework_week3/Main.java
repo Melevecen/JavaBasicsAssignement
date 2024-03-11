822a26f80006eea8b23f4c9156fb8552edc73f73
Melih Evecen <162025356+Melevecen@users.noreply.github.com>
2024-03-11 16:27:44 +0300

    Add files via upload

---
+++ Homework_week3/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class NameSurnameQuestion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+ System.out.println("İnput Your Name ");
+  String name,surname;
+  name=input.nextLine(  );
+  System.out.println("İnput Your Surname");
+  surname=input.nextLine(  );
+
+  System.out.println(name+" "+surname);
+
+
+
+
+    }
+}
